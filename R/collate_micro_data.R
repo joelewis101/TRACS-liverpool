1ed17b172bcb6882fda25359f94dfc96e5a4fb31
Joe Lewis <joelewis101@doctors.org.uk>
2024-08-27 21:36:57 +0100

    Add indexing to logfile to improve bugfix

--- R/collate_micro_data.R
+++ R/collate_micro_data.R
@@ -52,11 +52,11 @@ listout <- list()
 prev_samples_listout <- list()
 
 for (sample_file in sample_files) {
-  cat("\nWorking on ", sample_file, "\n")
+  cat("[",i,"] Working on ", sample_file, "\n")
   location <- gsub("/.*$", "", sample_file)
   week <- str_extract(sample_file, "Week [0-9]*")
-  cat("Location: ", location, "\n")
-  cat("Week: ", week)
+  cat("[",i,"] Location: ", location, "\n")
+  cat("[",i,"] Week: ", week, "\n")
 
   sheets <- excel_sheets(here("data/raw/Sampling data", sample_file))
 
@@ -73,14 +73,14 @@ for (sample_file in sample_files) {
     stop(paste0("Wrong number of sheets in ", sample_file))
   }
 
-  cat("Receipt sheets: ", receipt_sheets, "\n")
+  cat("[",i, "] Receipt sheets: ", receipt_sheets, "\n")
 
   receipt_df <-
     read_xlsx(here("data/raw/Sampling data", sample_file), sheet = receipt_sheets) |>
     janitor::clean_names()
   head(receipt_df) |> print()
 
-  cat("\nPlating sheets: ", plating_sheets, "\n")
+  cat("\n[",i, "] Plating sheets: ", plating_sheets, "\n")
   plating_df <-
     read_xlsx(here("data/raw/Sampling data", sample_file), sheet = plating_sheets) |>
     janitor::clean_names()
@@ -98,13 +98,13 @@ for (sample_file in sample_files) {
       by = join_by(tracs_id)
     )
 
-  cat("\nqPCR result sheets: ", qpcr_sheets, "\n")
+  cat("\n[",i, "] qPCR result sheets: ", qpcr_sheets, "\n")
   if (any(grepl("#98 qPCR results", qpcr_sheets)) &
     sample_file == "Broad Green/Visit 1/Week 14_11.09.23/11.09.23 TRACS sample spreadsheet.xlsx") {
     # This is a retest of a sample that is carried over to the next spreadhseet -
     # needs a bit of manual cleaning
     # the sheet it's carried over to is 18.09.23\ TRACS\ sample\ spreadsheet.xlsx
-    cat("Dropping #98 qPCR results - it's a retest carried ovee to the next sheet.\n")
+    cat("[",i, "] Dropping #98 qPCR results - it's a retest carried ovee to the next sheet.\n")
     qpcr_sheets <- qpcr_sheets[!grepl("#98 qPCR results", qpcr_sheets)]
   }
 
@@ -132,11 +132,30 @@ for (sample_file in sample_files) {
       filter(grepl("[w|W]eek|wk", lab_id)) |>
       mutate(location = location, week_of_pcr = week)
 
-    cat("\n", nrow(prev_weeks_samples), "that look like they are qPCR reruns - removing to add back in at the end")
+    cat("\n[",i, "]", nrow(prev_weeks_samples), "that look like they are qPCR reruns - removing to add back in at the end\n")
 
-    cat(paste0(
-      "\nNumber of non-control records that don't start with a number or i , and will be dropped: ",
-      qpcr_results_df |>
+    # checked out i = 2
+    # Aintree/Visit 1_W29_8.05.23/Week 4_15.05.23/150523 TRACS sample spreadsheet.xlsx
+    # there are a number of samples marked #5 chrom etc - I have ignored  
+    # is this ok? Waht are they?
+
+    # i = 7 What are the samples in 18.09.23 TRACS sample spreadsheet Broad Green/Visit 1/Week 15_18.09.23 that start with SG - I guess Sarah’s samples?
+
+    # i = 17
+    # Estuary house/Visit 2_02.10.23/Week 16_02.10.23/02.10.23 TRACS sample log.xlsx
+    # a further 14 samples that lab id startes with SG - I guess more of sarah's
+    #samples?
+
+    # i = 27
+# Longmoor house/Visit 2_19.06.23/Week 5_19.06.23/19.06.23 TRACS sample spreadsheet.xlsx
+    # More SG samples - lab id started with SG
+
+    # i = 39
+# Whiston/Visit 1_6.11.23/Week 21_6.11.23/6.11.23 TRACS sample log.xlsx
+    # ther are lots of samples here where lab id starts BS or P - what are they?
+
+    cat(
+  paste0("[ ",i, " ] Number of non-control records that don't start with a number or i , and will be dropped: ", qpcr_results_df |>
         filter(!is.na(lab_id), !grepl("control", lab_id), !grepl("^[0-9]|^i", lab_id)) |>
         nrow(),
       "\n"
@@ -184,7 +203,7 @@ for (sample_file in sample_files) {
       as.data.frame()
   }
 
-  cat("\nMALDI result sheets: ", maldi_sheets, "\n")
+  cat("\n[",i, "] MALDI result sheets: ", maldi_sheets, "\n")
 
   if (length(maldi_sheets > 1)) {
     if (sample_file %in% c(
@@ -211,8 +230,8 @@ for (sample_file in sample_files) {
       map(maldi_results_list, \(x) print(head(x)))
     }
 
-    cat(paste0(
-      "\nNumber of non-control records that don't start with a number or i , and will be dropped: ",
+    cat("\n[",i, "]", paste0(
+      "Number of non-control records that don't start with a number or i , and will be dropped: ",
       
       maldi_results_list |> 
         bind_rows() |>
