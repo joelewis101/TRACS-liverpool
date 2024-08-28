f110fd38a86371a2ced9fb91658dc6aebc7a45ef
Joe Lewis <joelewis101@doctors.org.uk>
2024-08-28 15:31:51 +0100

    Add report of exit code to shell scripts

--- R/clean_redcap_data.sh
+++ R/clean_redcap_data.sh
@@ -3,4 +3,10 @@
 echo "Running clean_redcap_data.R"
 echo "Logfile is at ../data/processed/clean_data_log$(date '+%Y%m%d-%H%M').txt"
 RScript clean_redcap_data.R &> ../data/processed/clean_data_log$(date "+%Y%m%d-%H%M").txt
-echo "done"
+retval=$?
+if [ $retval -ne 0 ]
+then
+  echo "Failed with exit code " $retval " - check logs"
+else
+  echo "Completed, exit code" $retval
+fi
--- R/import_data_from_redcap.sh
+++ R/import_data_from_redcap.sh
@@ -3,4 +3,10 @@
 echo "Running import_data_from_redcap.R"
 echo "Logfile is at ../data/raw/redcap_extract_log$(date '+%Y%m%d-%H%M').txt"
 RScript import_data_from_redcap.R &> ../data/raw/redcap_extract_log$(date "+%Y%m%d-%H%M").txt
-echo "done"
+retval=$?
+if [ $retval -ne 0 ]
+then
+  echo "Failed with exit code " $retval " - check logs"
+else
+  echo "Completed, exit code" $retval
+fi
