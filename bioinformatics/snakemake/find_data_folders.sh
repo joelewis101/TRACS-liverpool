73962cad4bb41ef8d4aa2cd223f9cc8a9515c40d
Sarah Gallichan <sarah.gallichan@8afbf598.st-andrews.ac.uk>
2025-03-06 09:11:13 +0000

    Bash script to list path to data folder

--- bioinformatics/snakemake/find_data_folders.sh
+++ bioinformatics/snakemake/find_data_folders.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-# Define the root directory to search
-SEARCH_DIR="/Users/sarah.gallichan/Documents/Pipe_test/TRACS-liverpool/bioinformatics/snakemake"
-
-# Output header
-echo -e "Folder Name\tFull Path"
-
-# Find all "data" directories and format the output
-find "$SEARCH_DIR" -type d -name "data" | while read dir; do
-    echo -e "$(basename "$dir")\t$dir"
-done
+find . -type d -name "data" -maxdepth 4 | sort
