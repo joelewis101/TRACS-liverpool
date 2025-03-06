9d82a807231f41c3421642ede7d6323bccfd23d5
Sarah Gallichan <sarah.gallichan@8afbf598.st-andrews.ac.uk>
2025-03-06 09:24:41 +0000

    Bash script to list path to data folder and run snakemake

--- bioinformatics/snakemake/find_data_folders.sh
+++ bioinformatics/snakemake/find_data_folders.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-find . -type d -name "data" -maxdepth 4 | sort
+find . -type d -name "data" -maxdepth 4 | while read -r data_path; do
+parent_dir=$(dirname "$data_path")
+
+snakemake --config reads="$data_path" output="$parent_dir/out"
+done
