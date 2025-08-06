76fdb403444177565e4b0d5da81ec8b79752a7be
SarahGallichan <100708194+SarahGallichan@users.noreply.github.com>
2025-08-06 10:13:02 +0100

    Update find_data_folders.sh

--- bioinformatics/snakemake/find_data_folders.sh
+++ bioinformatics/snakemake/find_data_folders.sh
@@ -3,5 +3,5 @@
 find . -type d -name "data" -maxdepth 4 | while read -r data_path; do
 parent_dir=$(dirname "$data_path")
 
-snakemake --config reads="$data_path" output="$parent_dir/out" -R --until generate_pick_masks
+snakemake --config reads="$data_path" output="$parent_dir/out" -R --rerun-incomplete
 done
